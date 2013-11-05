972decf69c1dfaf4be1e5d64c29d1833fa274ef2
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-11-05 13:09:08 +0600

    Initial implementation

---
+++ index.js
@@ -0,0 +1,128 @@
+
+/**
+ * Module dependencies.
+ */
+
+var type = require('type');
+
+/**
+ * Local variables.
+ */
+
+var BrowserSet = window.Set;
+var isNative = false;
+
+// Check native Set support
+if (BrowserSet) {
+  var s = new BrowserSet([1, 2]); // Chrome's Set does not have initial values support
+  isNative = s.size == 2;
+}
+
+/**
+ * Expose `Set`. Use ES6 Set when possible.
+ */
+
+module.exports = isNative ? BrowserSet : Set;
+
+/**
+ * String optimized `Set` polyfill.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ */
+
+function Set(values) {
+  if (values && !values.forEach)
+    throw new TypeError(values + ' is not iterable');
+
+  // set default values
+  this.clear();
+
+  // iterate through initial values
+  if (values) values.forEach(this.add, this);
+}
+
+/**
+ * Add `val` to the Set.
+ *
+ * @param {Mixed} val
+ */
+
+Set.prototype.add = function(val) {
+  val = prepareVal(val);
+
+  if (isString(val)) {
+    this._obj[val] = true;
+  } else if(!this.has(val)) {
+    this._arr.push(val);
+  }
+};
+
+/**
+ * Check if this Set has `val`.
+ *
+ * @param {Mixed} val
+ * @return {Boolean}
+ */
+
+Set.prototype.has = function(val) {
+  val = prepareVal(val);
+  return isString(val) ? !! this._obj[val] : this._arr.indexOf(val) >= 0;
+};
+
+/**
+ * Delete `val`.
+ *
+ * @param {Mixed} val
+ */
+
+Set.prototype.delete = function(val) {
+  val = prepareVal(val);
+
+  if (isString(val)) {
+    delete this._obj[val];
+  } else {
+    var index = this._arr.indexOf(val);
+    if (~index) this._arr.splice(index, 1);
+  }
+};
+
+/**
+ * Empty Set.
+ */
+
+Set.prototype.clear = function(){
+  this._obj = Object.create(null);
+  this._arr = [];
+};
+
+/**
+ * Returns the number of values in this Set.
+ */
+
+Object.defineProperty(Set.prototype, 'size', {
+  get: function() {
+    return this._arr.length + Object.keys(this._obj).length;
+  }
+});
+
+/**
+ * Type checking helpers
+ */
+
+// check type to use object container
+function isString(val) {
+  return typeof val == 'string';
+}
+
+// prepare value to be stored as an object key
+function prepareVal(val) {
+  switch (type(val)) {
+    case 'string': return '$' + val;
+    case 'number':
+    case 'date':
+    case 'regexp':
+    case 'boolean':
+    case 'null':
+    case 'undefined': return '' + val;
+    default: return val;
+  }
+}
---
+++ test/index-test.js
@@ -0,0 +1,65 @@
+describe('Set', function() {
+  var Set = require('set');
+  var expect = require('chai').expect;
+
+  it('returns a new set', function() {
+    var set = new Set();
+    expect(set instanceof Set).true;
+    expect(set.size).equal(0);
+  });
+
+  it('adds the value only once', function() {
+    var set = new Set();
+    set.add('hello');
+    set.add('hello');
+    set.add('there');
+
+    expect(set.has('hello')).true;
+    expect(set.has('there')).true;
+    expect(set.size).equal(2);
+  });
+
+  it('works with all type of data', function() {
+    var set = new Set();
+    var date = new Date();
+    var array = [1, 2];
+
+    set.add(undefined);
+    set.add(true);
+    set.add(null);
+    set.add(2);
+    set.add(date);
+    set.add(array);
+    set.add(setTimeout);
+
+    expect(set.has(undefined)).true;
+    expect(set.has('undefined')).false;
+    expect(set.has(true)).true;
+    expect(set.has('true')).false;
+    expect(set.has(null)).true;
+    expect(set.has('null')).false;
+    expect(set.has(2)).true;
+    expect(set.has('2')).false;
+    expect(set.has(date)).true;
+    expect(set.has(date.toString())).false;
+    expect(set.has(array)).true;
+    expect(set.has(setTimeout)).true;
+  });
+
+  it('has methods to delete values', function() {
+    var set = new Set();
+    var date = new Date();
+    set.add('hello');
+    set.add(date);
+    set.add(1);
+    set.add(true);
+
+    expect(set.size).equal(4);
+    set.delete(1);
+    set.delete('hello');
+    set.delete('hello');
+    expect(set.size).equal(2);
+    set.clear();
+    expect(set.size).equal(0);
+  });
+});
